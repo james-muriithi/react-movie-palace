8e79666fa3f67c7be82b6805763987033808b632
james-muriithi <muriithijames556@gmail.com>
2020-06-23 20:05:02 +0300

    header functionality

--- src/components/header/Search.js
+++ src/components/header/Search.js
@@ -1,24 +1,31 @@
-import React from "react";
-import PropTypes from 'prop-types'
+import React, {useRef, useEffect} from "react";
+import PropTypes from 'prop-types';
 
-const Search = ({isOpen}) => (
-    <form action="/search/" className={`header__search ${isOpen ? 'header__search--active' : ''}`}>
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="header__search-content">
-                        <input type="text" id="query" aria-label="search movie or series" 
-                            autoComplete="off" placeholder="Search for a movie, TV Series that you are looking for" name="query" />
-                        <button type="submit" aria-label="search" className="text-center">
-                            <span>search</span>
-                            <i className="icon ion-ios-search fs-24 d-none"></i>
-                        </button>
+
+const Search = ({isOpen}) => {
+    const searchInput = useRef(null);
+    useEffect(() => {
+        isOpen && searchInput.current.focus();
+    })
+    return (
+        <form action="/search/" className={`header__search ${isOpen ? 'header__search--active' : ''}`}>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="header__search-content">
+                            <input type="text" id="query" aria-label="search movie or series" ref={searchInput}
+                                autoComplete="off" placeholder="Search for a movie, TV Series that you are looking for" name="query" />
+                            <button type="submit" aria-label="search" className="text-center">
+                                <span>search</span>
+                                <i className="icon ion-ios-search fs-24 d-none"></i>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </form>
-)
+        </form>
+    );
+}
 
 Search.defaultProps = {
     isOpen: false
