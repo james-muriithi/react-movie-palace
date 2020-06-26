86a5d7b805337c62d6f520ce54c8df9848b237fe
james-muriithi <muriithijames556@gmail.com>
2020-06-27 02:46:36 +0300

    removed gatsby files

--- gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-const $ = require("jquery")
-
-export const onInitialClientRender = () => {
-    $(document).ready(function () {
-        console.log("The answer is don't think about it!")
-    });
-}
